96776e1af1a5eddf1d53373b7936c79918d99775
yang-xiao-ying <yang.xiaoying1@outlook.com>
2019-09-04 00:12:16 +0800

    three commit

--- dynamic/dongtu.py
+++ dynamic/dongtu.py
@@ -4,12 +4,13 @@ from PIL import Image
 def dynamic():
     image_list = []
     # 图片命名的时候，采用 数字.png ,方便str(i + 1)取
-    for i in range(6):
-        path = 'C:\\Users\\Administrator\\Desktop\\trend\\{}.png'.format(i + 1)
+    for i in range(11):
+        path = 'D:\\otate\\{}.png'.format(i + 1)
+
         img = Image.open(path)
         image_list.append(img)
 
-    list_path = 'C:\\Users\\Administrator\\Desktop\\trend\\dynamic.gif'
+    list_path = 'D:\\otate\\dynamic.gif'
     # duration 指的是动图每两张之间的时间间隔，其他的设置我是照抄的
     img.save(list_path, save_all=True, append_images=image_list, duration=1000)
 
